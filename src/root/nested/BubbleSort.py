fa51307f17ccea7ff0929f6d335ecb53d03c04b0
Linden Holt <ace.holt@gmail.com>
2018-05-18 10:40:15 -0400

    Init Commit
    Bubblesort works!

---
+++ src/root/nested/BubbleSort.py
@@ -0,0 +1,34 @@
+'''
+Created on May 17, 2018
+
+@author: Linden Holt
+'''
+import random
+#bubblesort function definition
+
+def bubbleSort (arr):
+    while True:
+        swap = False;
+        for i in range(len(arr)-1):
+            if arr[i] > arr[i+1]:
+                temp = arr[i]
+                arr[i] = arr[i+1]
+                arr[i+1] = temp
+                swap = True
+        if swap == False:
+            break
+        
+    
+    return[arr]
+
+#populates array with random integers, calls bubblesort function
+
+arr = []
+for i in range(10):
+    arr.append(random.randint(1,100))
+
+print(arr)
+print(bubbleSort(arr))
+
+
+    
